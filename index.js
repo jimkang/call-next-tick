9f2a279c4327dc10595ef2e4e22510b9eaaec0e5
Jim Kang <jimkang@gmail.com>
2017-03-11 13:50:42 -0500

    Tabs => spaces

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var sliceCall = Array.prototype.slice.call;
 
 // Expecting params cb, error, result1, result2, ...
 function makeCallbackCaller(cb) {
-	var paramsForCallback = Array.prototype.slice.call(arguments, 1);
+  var paramsForCallback = Array.prototype.slice.call(arguments, 1);
 
-	return function callbackCall() {
-		cb.apply(cb, paramsForCallback);
-	};
+  return function callbackCall() {
+    cb.apply(cb, paramsForCallback);
+  };
 }
 
 // Expecting params cb, error, result1, result2, ...
 function callNextTick() {
-	var caller = makeCallbackCaller.apply(
-		null, Array.prototype.slice.call(arguments, 0)
-	);
-	process.nextTick(caller);
+  var caller = makeCallbackCaller.apply(
+    null, Array.prototype.slice.call(arguments, 0)
+  );
+  process.nextTick(caller);
 }
 
 module.exports = callNextTick;
